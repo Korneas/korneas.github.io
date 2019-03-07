6948eab03e9633863df65da6e0763f8c0c918134
Camilo Montoya <cajomo0311@gmail.com>
2019-03-07 13:39:47 -0500

    Adding stuff to animate projects

--- js/main.js
+++ js/main.js
@@ -1,11 +1,14 @@
 //Developed by: Camilo Jose Montoya | @korneas
 
+
+//          MENU
+
 $('#menu').hide();
 
 var preloadTimer = setTimeout(() => {
-    $('#k_awaken').attr("src","img/k_awake.svg");
+    $('#k_awaken').attr("src", "img/k_awake.svg");
     $('#loader-text').text("READY!");
-    
+
 }, 3000);
 
 $(window).on('load', () => {
@@ -51,6 +54,71 @@ $('#aboutlink').on('click', () => {
 $('#contactlink').on('click', () => {
     toggleMenu();
 });
+
+//          PROJECT
+
+var counter = 0;
+var updateRate = 10;
+var timeUpdate = function () {
+    return counter++ % updateRate === 0;
+}
+
+var project = document.getElementsByClassName('project');
+var inner = document.getElementsByClassName('inner-project');
+
+var onMouseEnterHandler = function (event) {
+    update(event);
+};
+
+var onMouseLeaveHandler = function () {
+    inner.style = "";
+};
+
+var onMouseMoveHandler = function (event) {
+    if (timeUpdate()) {
+        update(event);
+    }
+};
+
+project.onmouseenter = onMouseEnterHandler;
+project.onmouseleave = onMouseLeaveHandler;
+project.onmousemove = onMouseMoveHandler;
+
+var mouse = {
+    _x: 0,
+    _y: 0,
+    x: 0,
+    y: 0,
+    updatePosition: function (event) {
+        var e = event || window.event;
+        this.x = e.clientX - this._x;
+        this.y = (e.clientX - this._y) * -1;
+    },
+    setOrigin: function (e) {
+        this._x = e.offsetLeft + Math.floor(e.offsetWidth / 2);
+        this._y = e.offsetTop + Math.floor(e.offsetHeight / 2);
+    },
+    show: function (e) { return '(' + this.x + ', ' + this.y + ')'; }
+}
+
+mouse.setOrigin(container);
+
+var update = function (event) {
+    mouse.updatePosition(event);
+    updateTransformStyle(
+        (mouse.y / inner.offsetHeight / 2).toFixed(2),
+        (mouse.x / inner.offsetWidth / 2).toFixed(2)
+    );
+};
+
+var updateTransformStyle = function (x, y) {
+    var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+    inner.style.transform = style;
+    inner.style.webkitTransform = style;
+    inner.style.mozTransform = style;
+    inner.style.msTransform = style;
+    inner.style.oTransform = style;
+}
 
 // Calling Behance API
 
